316bf0426ab16a849dae09d3be6f82f93195be16
Tite <joe@palparty.com>
2018-11-18 20:45:35 -0500

    Changed cartridge to actually be a PHP builder.

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php require_once "main-web.phar"; ?>
+<?php 
+
+$main_phar="main-web.phar";
+
+if (file_exists($main_phar)) {
+	require_once $main_phar; 
+} else {
+	?>
+<html><body><h1>Application not Initialized!</h1>(<?php echo $main_phar; ?> not found)</body></html>
+	<?php
+}
+?>
